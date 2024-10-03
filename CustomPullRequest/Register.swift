d1adb819bca43d6afd19709fff8018758dcfbe09
Rushali Patel <rushali.ios.bt@gmail.com>
2024-10-03 14:50:15 +0530

    register

---
+++ CustomPullRequest/Register.swift
@@ -0,0 +1,18 @@
+//
+//  Register.swift
+//  CustomPullRequest
+//
+//  Created by Rushali's iMac on 03/10/24.
+//
+
+import SwiftUI
+
+struct Register: View {
+    var body: some View {
+        Text("Register")
+    }
+}
+
+#Preview {
+    Register()
+}
