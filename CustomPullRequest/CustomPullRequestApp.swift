ceeb2994381f3414a182cb8d4babdfc7cd6ec834
Rushali Patel <rushali.ios.bt@gmail.com>
2024-10-03 10:53:29 +0530

    Init Project

---
+++ CustomPullRequest/CustomPullRequestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPullRequestApp.swift
+//  CustomPullRequest
+//
+//  Created by Rushali's iMac on 03/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomPullRequestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
